94d52af0ed7fef161ecddc6ac94c1d9eb0948464
PC <truongvanhao159@gmail.com>
2023-12-19 19:58:06 +0700

    Double jump + walk

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,68 +1,13 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    private float moveSpeed = 3f;
-    private PlayerInputActions playerInputActions;
-    private Rigidbody2D rigidBody = null;
-    private Animator animator = null;
-
-    private void Awake()
+    void Start()
     {
-        playerInputActions = new PlayerInputActions();
-        rigidBody = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
     }
 
-    private void OnEnable()
+    void Update()
     {
-        playerInputActions.Enable();
-        playerInputActions.Player.Move.performed += OnMovementPerformed;
-        playerInputActions.Player.Move.canceled += OnMovementCanceled;
-    }
 
-    private void OnDisable()
-    {
-        playerInputActions.Disable();
-        playerInputActions.Player.Move.performed -= OnMovementPerformed;
-        playerInputActions.Player.Move.canceled -= OnMovementCanceled;
-    }
-
-    private void Start()
-    {
-        Flip(true);
-    }
-
-    private void OnMovementPerformed(InputAction.CallbackContext context)
-    {
-        animator.SetBool("isRun", true);
-        var moveVector = context.ReadValue<Vector2>();
-        moveVector.y = 0f; // only move left or right
-
-        if (moveVector.x > 0f)
-        {
-            Flip(true);
-        }
-        else
-        {
-            Flip(false);
-        }
-
-        rigidBody.velocity = moveVector * moveSpeed;
-    }
-
-    private void OnMovementCanceled(InputAction.CallbackContext context)
-    {
-        animator.SetBool("isRun", false);
-        rigidBody.velocity = Vector2.zero * moveSpeed;
-    }
-
-    private void Flip(bool isFacingRight)
-    {
-        Vector3 scale = transform.localScale;
-        scale.x = Mathf.Sign(isFacingRight ? 1f : -1f);
-
-        rigidBody.transform.localScale = scale;
     }
 }
--- Assets/Scripts/Player/PlayerAnimator.cs
+++ Assets/Scripts/Player/PlayerAnimator.cs
@@ -1,18 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerAnimator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Animator animator = null;
+
+    void Awake()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void RunAnimation(bool isRunning)
     {
-        
+        animator.SetBool("isRun", isRunning);
+    }
+
+    public void JumpAnimation(bool isJump)
+    {
+        animator.SetFloat("JumpY", isJump ? 2f : 0.1f);
+    }
+
+    public void WalkAnimation(bool isWalk)
+    {
+        animator.SetBool("isWalk", isWalk);
     }
 }
